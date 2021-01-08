288c2846e4ced6147784b26ee626b77963358430
Alexander Yastrebov <alexander@Someones-MacBook-Air.local>
2021-01-08 22:22:12 +0300

    ex00 done

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -3,17 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kcedra <kcedra@student.42.fr>              +#+  +:+       +#+        */
+/*   By: alexander <alexander@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 17:43:53 by kcedra            #+#    #+#             */
-/*   Updated: 2021/01/07 18:34:19 by kcedra           ###   ########.fr       */
+/*   Updated: 2021/01/08 22:20:51 by alexander        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
+//#include <string>
+
+char	*strToUpper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		str[i] = std::toupper(str[i]);
+		i++;
+	}
+	return (str);
+}
 
 int	main(int argc, char **argv)
 {
-	
+	int	i;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (i < argc)
+			std::cout << strToUpper(argv[i++]);
+		std::cout << std::endl;
+	}
 }
