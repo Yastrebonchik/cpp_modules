fa05b9765d229d46671d0d7b6a67bb1652113ed6
Alexander Yastrebov <alexander@Someones-MacBook-Air.local>
2021-01-13 23:26:46 +0300

    All is working, it's just Windows problem

--- day01/ex06/Weapon.cpp
+++ day01/ex06/Weapon.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Weapon.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: alexander <alexander@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 17:06:58 by marvin            #+#    #+#             */
-/*   Updated: 2021/01/13 17:06:58 by marvin           ###   ########.fr       */
+/*   Updated: 2021/01/13 23:24:16 by alexander        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 Weapon::Weapon(std::string type) : _type(type)
 {
-	std::cout << "Weapon " << _type << " created" << std::endl;
+	//std::cout << "Weapon " << _type << " created" << std::endl;
 }
 
 Weapon::~Weapon()
 {
-	std::cout << "Weapon destroyed" << std::endl;
+	//std::cout << "Weapon destroyed" << std::endl;
 }
 
 std::string &	Weapon::getType()
